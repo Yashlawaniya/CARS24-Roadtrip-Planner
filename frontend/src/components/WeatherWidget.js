6f5c950798bc81d445282ff100c502f710ba2ab1
Yash Lawaniya <yashlawaniya2804@gmail.com>
2025-09-12 15:43:46 +0530

    feat: Prepared

--- frontend/src/components/WeatherWidget.js
+++ frontend/src/components/WeatherWidget.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BASE_URL } from '../api';
+
 
 const WeatherWidget = ({ location }) => {
     const [weather, setWeather] = useState(null);
